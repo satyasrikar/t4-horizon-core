63ab5038ec8c797f1047196f75b41c09b8e316a9
Satyasri Kar <satya.kar98@gmail.com>
2021-10-12 16:13:40 +0530

    OpenApi 3.0 Swagger UI Added

--- src/main/kotlin/com/horizon/core/HorizonCoreApplication.kt
+++ src/main/kotlin/com/horizon/core/HorizonCoreApplication.kt
@@ -2,8 +2,17 @@ package com.horizon.core
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Info
 
 @SpringBootApplication
+@OpenAPIDefinition(
+	info = Info(
+		title = "Horizon Core API",
+		version = "1.1.6",
+		description = "Information regarding Horizon Core Authentication APIs"
+	)
+)
 class HorizonCoreApplication
 
 fun main(args: Array<String>) {

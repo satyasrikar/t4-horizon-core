6810b4aaae349b6470fc6c4b2b2d6307538f261e
Satyasri Kar <satya.kar98@gmail.com>
2021-10-12 16:04:38 +0530

    initial commit - master

---
+++ src/main/kotlin/com/horizon/core/HorizonCoreApplication.kt
@@ -0,0 +1,11 @@
+package com.horizon.core
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HorizonCoreApplication
+
+fun main(args: Array<String>) {
+	runApplication<HorizonCoreApplication>(*args)
+}
---
+++ src/test/kotlin/com/horizon/core/HorizonCoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.horizon.core
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HorizonCoreApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
